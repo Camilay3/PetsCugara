5b472aaf788831bd0e5ee5aec0edc91339c41285
Camila Azevedo <camila.santiago026@gmail.com>
2022-11-03 14:56:14 -0300

    botoes

--- PetsCugara/Pages/FirstPage.swift
+++ PetsCugara/Pages/FirstPage.swift
@@ -10,49 +10,57 @@ import SwiftUI
 struct FirstPage: View {
     var body: some View {
         ZStack {
-            Color(red: 145 / 255, green: 84 / 255, blue: 40 / 255)
-            
-            Rectangle()
-                .fill(Color(red: 137 / 255, green: 76 / 255, blue: 32 / 255))
-                .frame(width: 400, height: 227)
-                .position(x: 400/2, y: 180)
-            
-            Rectangle()
-                .fill(Color(red: 120 / 255, green: 61 / 255, blue: 18 / 255))
-                .frame(width: 400, height: 227)
-                .position(x: 400/2, y: 160)
-            
-            Rectangle()
-                .fill(Color(red: 91 / 255, green: 43 / 255, blue: 9 / 255))
-                .frame(width: 400, height: 227)
-                .position(x: 400/2, y: 140)
-            
-            Rectangle()
-                .fill(Color(red: 66 / 255, green: 29 / 255, blue: 2 / 255))
-                .frame(width: 400, height: 227)
-                .position(x: 400/2, y: 120)
-            
-            Rectangle()
-                .fill(Color(red: 51 / 255, green: 27 / 255, blue: 9 / 255))
-                .frame(width: 400, height: 227)
-                .position(x: 400/2, y: 100)
-            
-            Text("Pets Cugara")
-                .bold()
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
-                .frame(width: 400, height: 227)
-                .position(x: 400/2, y: 227/2)
-                .foregroundColor(Color(red: 255/255, green: 183/255, blue: 98/255))
-//                .font("BubblegumSans-Regular")
-            
-            HStack {
-                Image("mascote")
-                    .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
-                
-                Text("- Bem vindo(a) ao Pets Cugara! Meu nome é lina e vou ser seu guia! Gostaria de acessar o tutorial do nosso aplicativo?")
-            }
-        }.ignoresSafeArea()
+			Color(red: 145 / 255, green: 84 / 255, blue: 40 / 255)
+			ZStack{
+				Rectangle()
+					.fill(Color(red: 137 / 255, green: 76 / 255, blue: 32 / 255))
+					.frame(width: 400, height: 227)
+					.position(x: 400/2, y: 180)
+				
+				Rectangle()
+					.fill(Color(red: 120 / 255, green: 61 / 255, blue: 18 / 255))
+					.frame(width: 400, height: 227)
+					.position(x: 400/2, y: 160)
+				
+				Rectangle()
+					.fill(Color(red: 91 / 255, green: 43 / 255, blue: 9 / 255))
+					.frame(width: 400, height: 227)
+					.position(x: 400/2, y: 140)
+				
+				Rectangle()
+					.fill(Color(red: 66 / 255, green: 29 / 255, blue: 2 / 255))
+					.frame(width: 400, height: 227)
+					.position(x: 400/2, y: 120)
+				
+				Rectangle()
+					.fill(Color(red: 51 / 255, green: 27 / 255, blue: 9 / 255))
+					.frame(width: 400, height: 227)
+					.position(x: 400/2, y: 100)
+				
+				Text("Pets Cugara")
+					.bold()
+					.font(.largeTitle)
+					.multilineTextAlignment(.center)
+					.frame(width: 400, height: 227)
+					.position(x: 400/2, y: 227/2)
+					.foregroundColor(Color(red: 255/255, green: 183/255, blue: 98/255))
+//					.font("BubblegumSans-Regular")
+			}
+			
+			VStack{
+				HStack {
+					Image("mascote")
+						.rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
+					
+					Text("- Bem vindo(a) ao Pets Cugara! Meu nome é lina e vou ser seu guia! Gostaria de acessar o tutorial do nosso aplicativo?")
+						.foregroundColor(.white)
+						.padding(15)
+				}
+				
+				Button("Sim") {}
+				Button("Pular Tutorial") {}
+			}
+		}.ignoresSafeArea()
     }
 }
 
---
+++ PetsCugara/Pages/Tutorial/TutOne.swift
@@ -0,0 +1,20 @@
+//
+//  TutOne.swift
+//  PetsCugara
+//
+//  Created by Camila Azevedo do Vale Santiago on 03/11/22.
+//
+
+import SwiftUI
+
+struct TutOne: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TutOne_Previews: PreviewProvider {
+    static var previews: some View {
+        TutOne()
+    }
+}
