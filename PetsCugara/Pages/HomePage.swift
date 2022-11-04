3e7f81c696dfe70fe1e671d74f0c0755dc55d779
Camila Azevedo <camila.santiago026@gmail.com>
2022-11-04 14:43:41 -0300

    Head HomePage

--- PetsCugara/Pages/HomePage.swift
+++ PetsCugara/Pages/HomePage.swift
@@ -9,8 +9,57 @@ import SwiftUI
 
 struct HomePage: View {
     var body: some View {
-		Color(red: 145 / 255, green: 84 / 255, blue: 40 / 255)
-    }
+		ZStack{
+			Color(red: 145 / 255, green: 84 / 255, blue: 40 / 255)
+			
+			ZStack{
+				Rectangle()
+					.frame(width: 400, height: 180)
+					.foregroundColor(Color(red: 255 / 255, green: 223 / 255, blue: 200 / 255))
+					.shadow(radius: 10)
+					.position(x: 200, y: 90)
+				
+				HStack{
+					Image("mascote")
+						.scaleEffect(0.5)
+						.frame(width: 60, height: 100)
+						.padding(10)
+	
+					
+					Text("Tópicos")
+						.foregroundColor(Color(red: 68 / 255, green: 176 / 255, blue: 255 / 255))
+						.bold()
+						.font(.largeTitle)
+						.padding(5)
+					
+					Spacer()
+					
+					// Transformar em botão
+					Image(systemName: "exclamationmark.triangle.fill")
+						.foregroundColor(Color(red: 51 / 255, green: 27 / 255, blue: 9 / 255))
+						.scaleEffect(2)
+						.frame(width: 50, height: 40)
+						.padding(10)
+					
+				}.position(x: 200, y: 100)
+			}
+			
+			VStack{
+				HStack{
+					Image(systemName: "pawprint.fill")
+						.foregroundColor(Color(red: 255/255, green: 183/255, blue: 98/255))
+						.frame(width: 50, height: 50)
+						.scaleEffect(2.5)
+						.padding(20)
+					
+					Button("Banho"){
+						print("certo")
+					}.foregroundColor(.white)
+					
+				}.position(x: 80, y: 230)
+			}
+		}.ignoresSafeArea()
+	}
 }
 
 struct HomePage_Previews: PreviewProvider {
