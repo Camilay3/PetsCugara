1bc69e1a691f8d896197b0d23b5251a0f79a0550
Murilo Rodrigues Maia <murilo.maia62@aluno.ifce.edu.br>
2022-11-11 14:57:55 -0300

    Update FirstPage.swift

--- PetsCugara/Pages/FirstPage.swift
+++ PetsCugara/Pages/FirstPage.swift
@@ -13,6 +13,7 @@ let Cterciaria = Color(red: 55 / 255, green: 63 / 255, blue: 71 / 255);
 
 struct FirstPage: View {
     var body: some View {
+<<<<<<< Updated upstream
         ZStack {
             LinearGradient(colors: [Cterciaria, Cprincipal, Cprincipal], startPoint: .top, endPoint: .bottom)
             
@@ -52,6 +53,53 @@ struct FirstPage: View {
 			.accentColor(.white)
             
 		}.ignoresSafeArea().foregroundColor(Cprincipal)
+=======
+        GeometryReader{ proxy in
+            ZStack {
+                LinearGradient(colors: [Color(red: 51 / 255, green: 27 / 255, blue: 9 / 255), Color(red: 145 / 255, green: 84 / 255, blue: 40 / 255)], startPoint: .top, endPoint: .bottom)
+                
+                ZStack{
+                    Text("Pets Cugara")
+                        .bold()
+                        .font(.custom("Pulang", size: 40))
+                        .multilineTextAlignment(.center)
+                        .frame(width: 400, height: 227)
+                        .position(x: 400/2, y: 235/2)
+                        .foregroundColor(Color(red: 255/255, green: 183/255, blue: 98/255))
+                }
+                
+                VStack{
+                    HStack {
+                        Image("lina")
+                            .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
+                            .frame(width: 60, height: 50)
+                        
+                        Text("- Bem vindo(a) ao Pets Cugara! Meu nome é Lina e vou ser seu guia! Gostaria de acessar o tutorial do nosso aplicativo?")
+                            .foregroundColor(.white)
+                            .padding(15)
+                            .font(.custom("pointfree", size: 18))
+                    }
+                    .position(x: proxy.size.width/2, y: proxy.size.height/2)
+                    
+                    VStack{
+                        Button("Sim") {}
+                            .foregroundColor(Color(red: 51 / 255, green: 27 / 255, blue: 9 / 255))
+                            .padding(10)
+                            .font(.custom("pointfree", size: 18))
+                        
+                        Button("Pular Tutorial") {}
+                            .foregroundColor(Color(red: 51 / 255, green: 27 / 255, blue: 9 / 255))
+                            .padding(10)
+                            .font(.custom("pointfree", size: 18))
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .accentColor(Color(red: 255/255, green: 183/255, blue: 98/255))
+                    .position(x: proxy.size.width/2, y: proxy.size.height/3)
+                }
+                                
+            }.ignoresSafeArea()
+        }
+>>>>>>> Stashed changes
     }
 }
 
