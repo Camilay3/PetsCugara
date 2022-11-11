56e2c77d3383ce7e3e5dd0309a05a25e7bb57c3b
Camila Azevedo <camila.santiago026@gmail.com>
2022-11-11 14:54:44 -0300

    cores

---
+++ PetsCugara/Cores.swift
@@ -0,0 +1,23 @@
+//
+//  Cores.swift
+//  PetsCugara
+//
+//  Created by Camila Azevedo do Vale Santiago on 11/11/22.
+//
+
+import SwiftUI
+
+struct Cores: View {
+    var body: some View {
+		let Cprincipal = Color(red: 206 / 255, green: 255 / 255, blue: 185 / 255);
+		let Csecundaria = Color(red: 107 / 255, green: 144 / 255, blue: 128 / 255);
+		let Cterciaria = Color(red: 55 / 255, green: 63 / 255, blue: 71 / 255);
+		let Ccontraste = Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255);
+    }
+}
+
+struct Cores_Previews: PreviewProvider {
+    static var previews: some View {
+        Cores()
+    }
+}
--- PetsCugara/Pages/FirstPage.swift
+++ PetsCugara/Pages/FirstPage.swift
@@ -7,20 +7,23 @@
 
 import SwiftUI
 
+let Cprincipal = Color(red: 206 / 255, green: 255 / 255, blue: 185 / 255);
+let Csecundaria = Color(red: 107 / 255, green: 144 / 255, blue: 128 / 255);
+let Cterciaria = Color(red: 55 / 255, green: 63 / 255, blue: 71 / 255);
+
 struct FirstPage: View {
     var body: some View {
         ZStack {
-            LinearGradient(colors: [Color(red: 51 / 255, green: 27 / 255, blue: 9 / 255), Color(red: 145 / 255, green: 84 / 255, blue: 40 / 255)], startPoint: .top, endPoint: .bottom)
+            LinearGradient(colors: [Cterciaria, Cprincipal, Cprincipal], startPoint: .top, endPoint: .bottom)
             
             ZStack{
-                
                 Text("Pets Cugara")
                     .bold()
                     .font(.custom("Pulang", size: 40))
                     .multilineTextAlignment(.center)
                     .frame(width: 400, height: 227)
                     .position(x: 400/2, y: 235/2)
-                    .foregroundColor(Color(red: 255/255, green: 183/255, blue: 98/255))
+					.foregroundColor(.white)
             }
             
             VStack{
@@ -30,26 +33,25 @@ struct FirstPage: View {
                         .frame(width: 60, height: 50)
                     
                     Text("- Bem vindo(a) ao Pets Cugara! Meu nome é Lina e vou ser seu guia! Gostaria de acessar o tutorial do nosso aplicativo?")
-                        .foregroundColor(.white)
+                        .foregroundColor(.black)
                         .padding(15)
                         .font(.custom("pointfree", size: 18))
                 }
                 
                 Button("Sim") {}
-                    .foregroundColor(Color(red: 51 / 255, green: 27 / 255, blue: 9 / 255))
+					.foregroundColor(.black)
                     .padding(10)
                     .font(.custom("pointfree", size: 18))
                 
                 Button("Pular Tutorial") {}
-                    .foregroundColor(Color(red: 51 / 255, green: 27 / 255, blue: 9 / 255))
+					.foregroundColor(.black)
                     .padding(10)
                     .font(.custom("pointfree", size: 18))
-                
             }
-            .buttonStyle(.borderedProminent)
-            .accentColor(Color(red: 255/255, green: 183/255, blue: 98/255))
+			.buttonStyle(.borderedProminent)
+			.accentColor(.white)
             
-        }.ignoresSafeArea()
+		}.ignoresSafeArea().foregroundColor(Cprincipal)
     }
 }
 
