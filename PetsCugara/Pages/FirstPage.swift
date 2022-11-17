7158945003a406624b7c3e9b6eec05c875392bd1
Camila Azevedo <camila.santiago026@gmail.com>
2022-11-17 16:45:43 -0300

    FirstPage paddings

--- PetsCugara/Pages/FirstPage.swift
+++ PetsCugara/Pages/FirstPage.swift
@@ -10,32 +10,34 @@ import SwiftUI
 struct FirstPage: View {
     var body: some View {
         GeometryReader{ proxy in
+//			ajustar cálculos
             ZStack {
-				LinearGradient(colors: [Cor.Cterciaria, Cor.Cprimaria, Cor.Cprimaria], startPoint: .top, endPoint: .bottom)
-                
-                ZStack{
-                    Text("Pets Cugara")
-                        .bold()
-                        .font(.custom("Pulang", size: 40))
-                        .multilineTextAlignment(.center)
-                        .frame(width: 400, height: 227)
-                        .position(x: 400/2, y: 235/2)
-                        .foregroundColor(.white)
-                }
+				LinearGradient(colors: [Cor.Cterciaria, Cor.Cprimaria, Cor.Cprimaria], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
                 
                 VStack{
+					Text("Pets Cugara")
+						.bold()
+						.font(.custom("Pulang", size: 40))
+						.multilineTextAlignment(.center)
+						.foregroundColor(.white)
+						.padding(25)
+					
+					Spacer()
+					
                     HStack {
                         Image("lina")
                             .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
                             .frame(width: 60, height: 50)
+							.padding(5)
                         
                         Text("- Bem vindo(a) ao Pets Cugara! Meu nome é Lina e vou ser seu guia! Gostaria de acessar o tutorial do nosso aplicativo?")
                             .foregroundColor(.black)
-                            .padding(15)
                             .font(.custom("pointfree", size: 18))
                     }
-                    .position(x: proxy.size.width/2, y: proxy.size.height/2)
+						.padding(20)
                     
+					Spacer()
+					
                     VStack{
                         Button("Sim") {}
                             .foregroundColor(.black)
@@ -47,12 +49,12 @@ struct FirstPage: View {
                             .padding(10)
                             .font(.custom("pointfree", size: 18))
                     }
-                    .buttonBorderShape(.capsule)
-                    .buttonStyle(.borderedProminent)
-                    .accentColor(Cor.Csecundaria)
-                    .position(x: proxy.size.width/2, y: proxy.size.height/3)
-                }
-            }.ignoresSafeArea()
+						.buttonBorderShape(.capsule)
+						.buttonStyle(.borderedProminent)
+						.accentColor(.white)
+						.padding(20)
+				}
+            }
         }
     }
 }
