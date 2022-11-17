e66d6f6c0e30ab3760bf93622c82d59a9f8e801b
Camila Azevedo <camila.santiago026@gmail.com>
2022-11-17 17:10:14 -0300

    FirstPage Navigation
    Finalização da primeira página

--- PetsCugara/Pages/FirstPage.swift
+++ PetsCugara/Pages/FirstPage.swift
@@ -9,53 +9,56 @@ import SwiftUI
 
 struct FirstPage: View {
     var body: some View {
-        GeometryReader{ proxy in
-//			ajustar cálculos
-            ZStack {
-				LinearGradient(colors: [Cor.Cterciaria, Cor.Cprimaria, Cor.Cprimaria], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
-                
-                VStack{
-					Text("Pets Cugara")
-						.bold()
-						.font(.custom("Pulang", size: 40))
-						.multilineTextAlignment(.center)
-						.foregroundColor(.white)
-						.padding(25)
+		NavigationView{
+			GeometryReader{ proxy in
+				ZStack {
+					LinearGradient(colors: [Cor.Cterciaria, Cor.Cprimaria, Cor.Cprimaria], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
 					
-					Spacer()
-					
-                    HStack {
-                        Image("lina")
-                            .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
-                            .frame(width: 60, height: 50)
-							.padding(5)
-                        
-                        Text("- Bem vindo(a) ao Pets Cugara! Meu nome é Lina e vou ser seu guia! Gostaria de acessar o tutorial do nosso aplicativo?")
-                            .foregroundColor(.black)
-                            .font(.custom("pointfree", size: 18))
-                    }
+					VStack{
+						Text("Pets Cugara")
+							.bold()
+							.font(.custom("Pulang", size: 40))
+							.multilineTextAlignment(.center)
+							.foregroundColor(.white)
+							.padding(25)
+						
+						Spacer()
+						
+						HStack {
+							Image("lina")
+								.rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
+								.frame(width: 60, height: 50)
+								.padding(5)
+							
+							Text("- Bem vindo(a) ao Pets Cugara! Meu nome é Lina e vou ser sua guia! Gostaria de acessar o tutorial do nosso aplicativo?")
+								.foregroundColor(.black)
+								.font(.custom("pointfree", size: 18))
+						}
 						.padding(20)
-                    
-					Spacer()
-					
-                    VStack{
-                        Button("Sim") {}
-                            .foregroundColor(.black)
-                            .padding(10)
-                            .font(.custom("pointfree", size: 18))
-                        
-                        Button("Pular Tutorial") {}
-                            .foregroundColor(.black)
-                            .padding(10)
-                            .font(.custom("pointfree", size: 18))
-                    }
+						
+						Spacer()
+						
+						VStack{
+							NavigationLink(destination: TutOne(), label: {
+								Text("Sim")
+							})
+							.padding(10)
+							
+							NavigationLink(destination: HomePage(), label: {
+								Text("Pular Tutorial")
+							})
+							.padding(10)
+						}
+						.foregroundColor(.black)
+						.font(.custom("pointfree", size: 18))
 						.buttonBorderShape(.capsule)
 						.buttonStyle(.borderedProminent)
 						.accentColor(.white)
 						.padding(20)
+					}
 				}
-            }
-        }
+			}
+		}
     }
 }
     
