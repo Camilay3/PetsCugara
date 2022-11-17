9dc3e40cb445c11fa7d047162948aac91e75e471
Camila Azevedo <camila.santiago026@gmail.com>
2022-11-17 13:55:40 -0300

    LoadPage

--- PetsCugara/Pages/LoadPage.swift
+++ PetsCugara/Pages/LoadPage.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct LoadPage: View {
     var body: some View {
-		ZStack{}
+		ZStack{
+			LinearGradient(colors: [Cor.Cterciaria, Cor.Cprimaria, Cor.Cprimaria], startPoint: .bottom, endPoint: .top)
+			
+			VStack{
+				Text("Pets Cugara")
+					.bold()
+					.font(.custom("Pulang", size: 60))
+					.padding(20)
+					.foregroundColor(Cor.Cterciaria)
+				
+				Image("lina")
+					.rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
+					.frame(width: 60, height: 50)
+					.scaleEffect(1.5)
+					.padding(50)
+			}
+//
+//			Spacer()
+//
+//			Text("Knight Wolves")
+//				.foregroundColor(.white)
+//			
+		}.ignoresSafeArea()
     }
 }
 
