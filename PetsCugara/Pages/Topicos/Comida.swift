12c5c77f6e1bc59625043bdd8a01acb61525d734
caiovieira <95643550+caioo1@users.noreply.github.com>
2022-11-22 17:28:02 -0300

    NavigationView nos Tópicos

---
+++ PetsCugara/Pages/Topicos/Comida.swift
@@ -0,0 +1,20 @@
+//
+//  Food.swift
+//  PetsCugara
+//
+//  Created by Caio Vieira Cajazeiras on 22/11/22.
+//
+
+import SwiftUI
+
+struct Comida: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Comida_Previews: PreviewProvider {
+    static var previews: some View {
+        Comida()
+    }
+}
---
+++ PetsCugara/Pages/Topicos/Curiosidades.swift
@@ -0,0 +1,20 @@
+//
+//  Food.swift
+//  PetsCugara
+//
+//  Created by Caio Vieira Cajazeiras on 22/11/22.
+//
+
+import SwiftUI
+
+struct Curiosidades: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Curiosidades_Previews: PreviewProvider {
+    static var previews: some View {
+        Curiosidades()
+    }
+}
