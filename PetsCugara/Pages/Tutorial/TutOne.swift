c4f55455a7cabed0ce4e28720fdbf7d2c3e7aece
Camila Azevedo <camila.santiago026@gmail.com>
2022-11-29 15:14:57 -0300

    TutOne

--- PetsCugara/Pages/Tutorial/TutOne.swift
+++ PetsCugara/Pages/Tutorial/TutOne.swift
@@ -8,13 +8,206 @@
 import SwiftUI
 
 struct TutOne: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	var body: some View {
+		NavigationView{
+			GeometryReader{ proxy in
+				ZStack{
+					ZStack{
+						Color("Cprimaria").ignoresSafeArea()
+						VStack{
+							ZStack{
+								Text("Tópicos")
+									.bold()
+									.font(.custom("Pulang", size: 45))
+									.multilineTextAlignment(.center)
+									.foregroundColor(Color("Cterciaria"))
+									.frame(width: 400, height: 227, alignment: .center)
+									.position(x: 370/2, y: 90/2)
+								
+								VStack{
+									HStack{
+										Spacer()
+										Button(action: {}, label: {
+											Image(systemName: "exclamationmark.triangle.fill")
+												.resizable()
+												.scaledToFit()
+												.frame(width: 50, height: 50)
+												.padding(25)
+												.foregroundColor(Color("Csecundaria"))
+										})
+									}
+									Spacer()
+								}
+								VStack{
+									HStack{
+										NavigationLink(destination: PaginaTopico(topico: .banho).navigationBarBackButtonHidden(true), label:{
+											VStack{
+												ZStack{
+													Rectangle()
+														.cornerRadius(21)
+														.frame(width: 120, height: 120)
+														.foregroundColor(.white)
+														.shadow(color: .gray, radius: 10)
+													Image("pet-shampoo")
+														.resizable()
+														.scaledToFit()
+														.frame(width: 84, height: 84)
+														.padding(.vertical, 14/10)
+														.padding(.horizontal, 140/10)
+													
+												}
+												Text("Banho")
+													.bold()
+													.font(.custom("pointfree", size: 18))
+													.foregroundColor(Color("Cterciaria"))
+											}
+											
+										}).padding(29)
+										
+										Button(action: {}, label:{
+											VStack{
+												ZStack{
+													Rectangle()
+														.cornerRadius(21)
+														.frame(width: 120, height: 120)
+														.foregroundColor(.white)
+														.shadow(color: .gray, radius: 10)
+													Image("pet-bowl-2-2")
+														.resizable()
+														.scaledToFit()
+														.frame(width: 80, height: 80)
+														.padding(.vertical, 14/10)
+														.padding(.horizontal, 140/10)
+												}
+												Text("Alimentação")
+													.bold()
+													.font(.custom("pointfree", size: 18))
+													.foregroundColor(Color("Cterciaria"))
+											}
+											
+										})
+										.padding(29)
+									}
+									HStack{
+										Button(action: {}, label: {
+											VStack{
+												ZStack{
+													Rectangle()
+														.cornerRadius(21)
+														.frame(width: 120, height: 120)
+														.foregroundColor(.white)
+														.shadow(color: .gray, radius: 10)
+													Image("vaccine-4")
+														.resizable()
+														.scaledToFit()
+														.frame(width: 80, height: 80)
+														.padding(.vertical, 14/10)
+														.padding(.horizontal, 140/10)
+												}
+												Text("Vacinação")
+													.bold()
+													.font(.custom("pointfree", size: 18))
+													.foregroundColor(Color("Cterciaria"))
+											}
+											
+										}).padding(29)
+										Button(action: {}, label:{
+											VStack{
+												ZStack{
+													Rectangle()
+														.cornerRadius(21)
+														.frame(width: 120, height: 120)
+														.foregroundColor(.white)
+														.shadow(color: .gray, radius: 10)
+													Image("ponto-de-interrogacao")
+														.resizable()
+														.scaledToFit()
+														.frame(width: 75, height: 75)
+														.padding(.vertical, 14/10)
+														.padding(.horizontal, 140/10)
+												}
+												Text("Curiosidades")
+													.bold()
+													.font(.custom("pointfree", size: 18))
+													.foregroundColor(Color("Cterciaria"))
+											}
+											
+										}).padding(29)
+									}
+									HStack{
+										Button(action: {}, label:{
+											VStack{
+												ZStack{
+													Rectangle()
+														.cornerRadius(21)
+														.frame(width: 120, height: 120)
+														.foregroundColor(.white)
+														.shadow(color: .gray, radius: 10)
+													Image("dog-2")
+														.resizable()
+														.scaledToFit()
+														.frame(width: 95, height: 95)
+														.padding(.vertical, 14/10)
+														.padding(.horizontal, 140/10)
+												}
+												Text("Passeio")
+													.bold()
+													.font(.custom("pointfree", size: 18))
+													.foregroundColor(Color("Cterciaria"))
+											}
+										}).padding(29)
+										Button(action: {}, label:{
+											VStack{
+												ZStack{
+													Rectangle()
+														.cornerRadius(21)
+														.frame(width: 120, height: 120)
+														.foregroundColor(.white)
+														.shadow(color: .gray, radius: 10)
+													Image("pet-shop-7")
+														.resizable()
+														.scaledToFit()
+														.frame(width: 80, height: 80)
+														.padding(.vertical, 14/10)
+														.padding(.horizontal, 140/10)
+												}
+												Text("Pet Shop")
+													.bold()
+													.font(.custom("pointfree", size: 18))
+													.foregroundColor(Color("Cterciaria"))
+											}
+											
+										}).padding(29)
+									}
+								}.position(x:200, y: 425)
+							}
+						}
+						
+						Color("Cfundo").ignoresSafeArea()
+							.opacity(0.8)
+	
+						VStack{
+							Spacer()
+							Text("- Bem vindo(a) ao tutorial! Essa é a página principal, onde você pode acessar os tópicos")
+								.foregroundColor(.black)
+								.padding(30)
+								.font(.custom("Pointfree", size: 17))
+								.background(Color(.white))
+								.cornerRadius(40)
+							
+							Image("lina")
+								.scaleEffect(1.2)
+								.padding(.vertical, 10)
+						}.padding(.horizontal, 20)
+					}
+				}
+			}
+		}
+	}
 }
 
 struct TutOne_Previews: PreviewProvider {
-    static var previews: some View {
-        TutOne()
-    }
+	static var previews: some View {
+		TutOne()
+	}
 }
