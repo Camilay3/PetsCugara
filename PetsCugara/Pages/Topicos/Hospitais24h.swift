39853909a77dec4967d30f8bd7f4d63e45579888
caiovieira <95643550+caioo1@users.noreply.github.com>
2022-12-01 11:20:48 -0300

    hospital24h

--- PetsCugara/Pages/Topicos/Hospitais24h.swift
+++ PetsCugara/Pages/Topicos/Hospitais24h.swift
@@ -9,18 +9,26 @@ import SwiftUI
 
 struct Hospitais24h: View {
     var body: some View {
-        GeometryReader{ proxy in
-            ZStack{
-                Color("Cprimaria").ignoresSafeArea()
-                VStack{
-                    HStack{
-                        Image(systemName: "pawprint.fill")
-                        Text("Hospitais 24h")
-                    }.padding(20)
-                    .font(.custom("Pulang", size: 35))
-                    .foregroundColor(Color("Cterciaria"))
-                    .padding(.vertical, 600/1000)
-                    .padding(.horizontal, 200/10)
+        NavigationView{
+            GeometryReader{ proxy in
+                ZStack{
+                    Color("Cprimaria").ignoresSafeArea()
+                    
+                    VStack{
+                        HStack{
+                            Image(systemName: "pawprint.fill")
+                            Text("Hospitais 24h")
+                            Spacer()
+                            NavigationLink(destination: HomePage(), label: {
+                                Image(systemName: "house.fill")
+                                    .foregroundColor(Color("Csecundaria"))
+                            })
+                        }
+                        .padding(20)
+                        .font(.custom("Pulang", size: 35))
+                        .foregroundColor(Color("Cterciaria"))
+                        .position(x: 190, y: 50)
+                    }
                 }
             }
         }
