5fd6055c7a0d4a5df8344d3563d94685b0f4bd93
Camila Azevedo <camila.santiago026@gmail.com>
2022-12-14 15:11:09 -0300

    ScrollView na descrição

--- Landmarkdetail.swift
+++ Landmarkdetail.swift
@@ -19,35 +19,37 @@ struct Landmarkdetail: View {
                 litlleimage(image: landmark.image)
                     .offset(y: -130)
                     .padding(.bottom, -130)
-                VStack(alignment: .leading){
-                    Text(landmark.name)
-                        .font(.custom("Pulang", size: 30))
-                        .foregroundColor(Color.black)
-                        .lineLimit(nil)
-                    Divider()
-                    Text(landmark.address)
-                        .font(.custom("Pulang", size: 20))
-                        .foregroundColor(Color("Cterciaria"))
-                    HStack{
-                        Text(landmark.city + " - " + landmark.state)
-                            .font(.custom("Pulang", size: 20))
-                            .foregroundColor(Color("Cterciaria"))
-                        Spacer()
-                            .font(.custom("Pulang", size: 20))
-                            .foregroundColor(Color("Cterciaria"))
-                    }
-                    Text("Bairro: " + landmark.bairro)
-                        .font(.custom("Pulang", size: 20))
-                        .foregroundColor(Color("Cterciaria"))
-                    Text("CEP: " + landmark.cep)
-                        .font(.custom("Pulang", size: 20))
-                        .foregroundColor(Color("Cterciaria"))
-                    Text("Contato: " + landmark.tele)
-                        .font(.custom("Pulang", size: 20))
-                        .foregroundColor(Color("Cterciaria"))
-                }
-                .padding()
-                Spacer()
+				ScrollView{
+					VStack(alignment: .leading){
+						Text(landmark.name)
+							.font(.custom("Pulang", size: 30))
+							.foregroundColor(Color.black)
+							.lineLimit(nil)
+						Divider()
+						Text(landmark.address)
+							.font(.custom("Pulang", size: 20))
+							.foregroundColor(Color("Cterciaria"))
+						HStack{
+							Text(landmark.city + " - " + landmark.state)
+								.font(.custom("Pulang", size: 20))
+								.foregroundColor(Color("Cterciaria"))
+							Spacer()
+								.font(.custom("Pulang", size: 20))
+								.foregroundColor(Color("Cterciaria"))
+						}
+						Text("Bairro: " + landmark.bairro)
+							.font(.custom("Pulang", size: 20))
+							.foregroundColor(Color("Cterciaria"))
+						Text("CEP: " + landmark.cep)
+							.font(.custom("Pulang", size: 20))
+							.foregroundColor(Color("Cterciaria"))
+						Text("Contato: " + landmark.tele)
+							.font(.custom("Pulang", size: 20))
+							.foregroundColor(Color("Cterciaria"))
+					}
+					.padding()
+					Spacer()
+				}
             }
             .background(Color("Cprimaria"))
         }
